149a713c6a5dc057bb2343065d89ac792bf860b6
Guilherme <guileisme1@gmail.com>
2024-07-30 13:27:22 -0300

    Add feature that read arq.txt and execute the instructions

--- atividade-01/ReadFromFileUsingScanner.java
+++ atividade-01/ReadFromFileUsingScanner.java
@@ -6,24 +6,48 @@ import java.util.Scanner;
 public class ReadFromFileUsingScanner {
     public static void main(String[] args) throws Exception
     {
-        // pass the path to the file as a parameter
+        // cria um novo objeto File para ser o arquivo .txt
         File file = new File("atividade-01\\arq.txt");
-        Scanner scanner = new Scanner(file);
-        ArrayList<String> lines = new ArrayList<String>();
+        //cria um scanner para ler o arquivo
+        Scanner scanner = new Scanner(file); 
+        // cria um array para separar cada linha do arquivo em strings
+        ArrayList<String> lines = new ArrayList<String>();  
 
+        // Condicional que fará o scanner ler todo o arquivo até que não haja nenhuma linha e que adiciona toda linha no array criado acima
         while (scanner.hasNextLine()){
             lines.add(scanner.nextLine());
-            
         }
+    
+        // remove a última linha do array porque ela é uma string vazia
         lines.remove(lines.getLast());
-        for (int i = 0; i < lines.size(); i++) {
-            // System.out.println(lines.get(i));
-        }
-        String [] teste = lines.get(0).split(" ");
-        ArrayList<String> definitiveList = new ArrayList<String>(Arrays.asList(teste));
+
+        // Separa a primeira linha do arquivo, contida no array "lines" e transforma a string em um array, usando o comando split para separar cada elemento e o separando em outro array, formando assim o array definido pelo arquivo .txt lido
+        String [] numbers = lines.get(0).split(" ");
+        ArrayList<String> definitiveList = new ArrayList<String>(Arrays.asList(numbers));
         lines.remove(0);
-        System.out.println(definitiveList);
-        System.out.println(lines);
+        
+        ArrayList<ArrayList<String>> instructionList = new ArrayList<ArrayList<String>>();
+
+        for (int idx = 1; idx < lines.size(); idx++) {
+            String [] line = lines.get(idx).split(" ");
+            ArrayList<String> instructions = new ArrayList<String>(Arrays.asList(line));
+            instructionList.add(instructions);
+        }
+
+        for (int i = 0; i < instructionList.size(); i++) {
+            if ("A".equals(instructionList.get(i).get(0))){
+                definitiveList.add(Integer.parseInt(instructionList.get(i).get(2)), instructionList.get(i).get(1));
+            }
+
+            if ("R".equals(instructionList.get(i).get(0))){
+                definitiveList.remove(instructionList.get(i).get(1));
+            }
+
+            if ("P".equals(instructionList.get(i).get(0))){
+                System.out.println(definitiveList);
+            }
+
+        }
         scanner.close();
     }
 }
